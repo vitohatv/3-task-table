dcc780f59895cf4bc8dd54f6f15b4629704f48e9
Vitaliy <vitohatv@gmail.com>
2024-03-23 03:03:54 +0500

    add css and table edited, 4 columns

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Skeleton, Table } from 'antd';
 import { useState } from 'react';
+import './table.css';
 
 function App() {
   const [data, setData] = useState([]);
@@ -12,19 +13,31 @@ function App() {
           key: '1',
           name: 'John Brown',
           age: 25,
+          hobby: 'Reading, painting',
           address: 'New York No. 1 Lake Park',
+          company: 'ABC Inc.',
+          email: 'johnDoee@gmail.com',
+          phone: '173-4096-7270',
         },
         {
           key: '2',
           name: 'Jim Green',
           age: 42,
+          hobby: 'Сoding',
           address: 'London No. 2 Lake Park',
+          company: 'XYZ Ltd.',
+          email: 'jimGree85@gmail.com',
+          phone: '185-3259-2211',
         },
         {
           key: '3',
           name: 'Joe Black',
           age: 37,
+          hobby: 'Fishing',
           address: 'Sidney No. 3 Lake Park',
+          company: '123 Corp.',
+          email: 'htdra3@gmail.com',
+          phone: '177-777-7771',
         },
       ]);
       setLoading(false);
@@ -33,28 +46,53 @@ function App() {
 
   const columns = [
     {
-      title: 'Name',
+      title: loading ? <Skeleton.Input active /> : 'Name',
       dataIndex: 'name',
       key: 'name',
       render: text => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text,
     },
     {
-      title: 'Age',
+      title: loading ? <Skeleton.Input active /> : 'Age',
       dataIndex: 'age',
       key: 'age',
       render: text => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text,
     },
     {
-      title: 'Address',
+      title: loading ? <Skeleton.Input active /> : 'Hobby',
+      dataIndex: 'hobby',
+      key: 'hobby',
+      render: (text) => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text,
+    },
+    {
+      title: loading ? <Skeleton.Input active /> : 'Address',
       dataIndex: 'address',
       key: 'address',
-      render: text => loading ? <Skeleton active paragraph={{ rows: 2 }} /> : text,
+      render: text => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text,
+    },
+    {
+      title: loading ? <Skeleton.Input active /> : 'Company',
+      dataIndex: 'company',
+      key: 'company',
+      render: (text) => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text,
+    },
+    {
+      title: loading ? <Skeleton.Input active /> : 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      render: (text) => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text,
+    },
+    {
+      title: loading ? <Skeleton.Input active /> : 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+      render: (text) => loading ? <Skeleton active paragraph={{ rows: 1 }} /> : text,
     },
   ];
+  
   tableData();
   return (
     <div className="App">
-      <Table columns={columns} dataSource={loading ? [{}] : data} />
+      <Table className='table' columns={columns} scroll={{x: '100%'}} dataSource={loading ? [{}] : data} />
     </div>
   );
 }
